870b23a1aad48725169adc6b3126c5a6f631f57d
regokan <regokan@gmail.com>
2024-11-09 16:09:43 +0530

    add module to handle openai gym environment

---
+++ env/constants.py
@@ -0,0 +1,10 @@
+"""Constants for the OpenAI Gym module."""
+
+_SUPPORTED_ENVIRONMENTS = {
+    "mountaincar": "MountainCar-v0",
+    "cliffwalking": "CliffWalking-v0",
+    "lunarlander": "LunarLander-v2",
+    "blackjack": "Blackjack-v1",
+    "frozenlake": "FrozenLake-v1",
+    "taxi": "Taxi-v3",
+}
