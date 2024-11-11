7597c7594b52dbce4355e186ffe1dd8736b86d6a
regokan <regokan@gmail.com>
2024-11-11 16:41:44 +0530

    add hill climbing trainer to the module

--- train/__init__.py
+++ train/__init__.py
@@ -1,6 +1,7 @@
 from .base import BaseTrainer
+from .hillClimbing import TrainerHillClimbing
 from .trainerDQN import TrainerDQN
 from .trainerMC import TrainerMC
 from .trainerTD import TrainerTD
 
-__all__ = ["TrainerMC", "TrainerTD", "TrainerDQN", "BaseTrainer"]
+__all__ = ["TrainerMC", "TrainerTD", "TrainerDQN", "TrainerHillClimbing", "BaseTrainer"]
